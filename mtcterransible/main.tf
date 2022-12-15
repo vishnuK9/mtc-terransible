5173933cc8bc3a7e302bb15c9ac18ae88599e285
vishnuk9 <vishnuacharya99.com>
2022-12-15 16:54:44 +0530

    aa

--- mtcterransible/main.tf
+++ mtcterransible/main.tf
@@ -1,10 +1,14 @@
+resource "random_id" "random" {
+  byte_length = 2
+}
+
 resource "aws_vpc" "mtc_vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    name = "mtc_vpc"
+    name = "mtc_vpc-${random_id.random.dec}"
   }
 }
 
