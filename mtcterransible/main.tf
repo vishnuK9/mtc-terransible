a84f08bb7af5ab6013f84903048c843a18cbc814
vishnuk9 <vishnuacharya99.com>
2022-12-15 17:26:01 +0530

    aa

--- mtcterransible/main.tf
+++ mtcterransible/main.tf
@@ -22,3 +22,24 @@ resource "aws_internet_gateway" "mtc_igw" {
     Name = "mtc_igw-${random_id.random.dec}"
   }
 }
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    "name" = "mtc-public"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_igw.id
+}
+
+resource "aws_route" "r" {
+  route_table_id            = "rtb-4fbb3ac4"
+  destination_cidr_block    = "10.0.1.0/22"
+  vpc_peering_connection_id = "pcx-45ff3dc1"
+  depends_on                = [aws_route_table.testing]
+}
