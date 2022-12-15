5121c55b30b2561f5dd21e28b43b35cef76ffd2f
vishnuk9 <vishnuacharya99.com>
2022-12-15 20:26:20 +0530

    aa

--- mtcterransible/compute.tf
+++ mtcterransible/compute.tf
@@ -7,3 +7,18 @@ data "aws_ami" "server_ami" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
+
+resource "aws_instance" "mtc_main" {
+  instance_type = var.main_instance_type
+  ami = data.aws_ami.server_ami.id
+  # key_name = ""
+  vpc_security_group_id = [aws_security_group.mtc_sg.id]
+  subnet_id = aws_subnet.mtc_public_subnet[0].id
+  root_block_device {
+    volume_size = var.main_vol_size
+  }
+  tags = {
+    "name" = "mtc_main"
+  }
+
+}
--- mtcterransible/variables.tf
+++ mtcterransible/variables.tf
@@ -8,3 +8,13 @@ variable "access_ip" {
   type = list(string)
   default = ["0.0.0.0/0"]
 }
+
+variable "main_instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "main_vol_size" {
+  type = number
+  default = 8
+}
