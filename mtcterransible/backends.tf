92b2625e47309809d2fe2a27fb8ea56eadc12683
vishnuk9 <vishnuacharya99.com>
2022-12-15 16:01:50 +0530

    s3 backend

--- mtcterransible/backends.tf
+++ mtcterransible/backends.tf
@@ -1,9 +1,7 @@
 terraform {
-  cloud {
-    organization = "OT_terransible"
-
-    workspaces {
-      name = "terransible"
-    }
+  backend "s3" {
+    bucket = "vprofile-kops-state-ap-south-1"
+    key = "OT/s3/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
