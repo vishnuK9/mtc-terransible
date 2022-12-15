a15be9b0229cc0488fda03c52b7e8d1556a4fe90
vishnuk9 <vishnuacharya99.com>
2022-12-15 16:44:54 +0530

    aa

--- mtcterransible/main.tf
+++ mtcterransible/main.tf
@@ -1,9 +1,17 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "mtc_vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
     name = "mtc_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_igw" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "mtc_igw"
   }
 }
--- mtcterransible/variables.tf
+++ mtcterransible/variables.tf
@@ -1,3 +1,5 @@
 variable "vpc_cidr" {
   type = string
+  description = "cidr value"
+  default = "10.123.0.0/16"
 }
