1c0b432c21ffaa0e69d91162c2be260f689f0c31
vishnuk9 <vishnuacharya99@gmail.com>
2023-02-01 13:24:47 +0530

    aa

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -39,8 +39,7 @@ variable "vpc_private_subnets" {
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
-  # default = ["10.0.151.0/24", "10.0.152.0/24"]
-  default = []
+  default = ["10.0.151.0/24", "10.0.152.0/24"]
 }
 
 # VPC Create Database Subnet Group (True / False)
