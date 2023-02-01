3402e3c41de0a80bef9c8740b78ba7757ee5d134
vishnuk9 <vishnuacharya99.com>
2023-02-02 00:36:52 +0530

    a

--- ec2-and-securityGroups/nullresource.tf
+++ ec2-and-securityGroups/nullresource.tf
@@ -2,7 +2,7 @@ resource "null_resource" "name" {
   depends_on = [module.ec2_public]
   connection {
     type = "ssh"
-    host = "${module.ec2_public[*].public_ip}"
+    host = "${module.ec2_public.public_ip}"
     user = "ec2-user"
     password = ""
     private_key = file("private-key/k8s.pem")
@@ -21,12 +21,6 @@ resource "null_resource" "name" {
 
   provisioner "local-exec" {
     command = "echo vpc created on `date` and vpc id : ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
-    working_dir = "local-exec-output-files/"
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "echo destroy time prov `date` >> destroy-time-prov.txt "
     working_dir = "local-exec-output-files/"
   }
 }
