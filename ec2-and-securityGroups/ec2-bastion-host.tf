1a20b8248999a0c856a0d268a06ee2a8c8745504
vishnuk9 <vishnuacharya99.com>
2023-02-02 01:56:22 +0530

    aa

--- ec2-and-securityGroups/ec2-bastion-host.tf
+++ ec2-and-securityGroups/ec2-bastion-host.tf
@@ -9,4 +9,17 @@ module "ec2_public" {
   subnet_id = data.terraform_remote_state.vpc.outputs.public_subnets[0]
   vpc_security_group_ids = [module.public_bastion_sg.this_security_group_id]
   tags = local.common_tags
+
+  provisioner "local-exec" {
+    command = "echo vpc created on `date` and vpc id : ${data.terraform_remote_state.vpc.outputs.vpc_id} >> creation-time-vpc-id.txt"
+    working_dir = "local-exec-output-files/"
+  }
+
+  ## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
+  provisioner "local-exec" {
+    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    working_dir = "local-exec-output-files/"
+    when = destroy
+    #on_failure = continue
+  }    
 }
--- ec2-and-securityGroups/nullresource.tf
+++ ec2-and-securityGroups/nullresource.tf
@@ -2,7 +2,7 @@ resource "null_resource" "name" {
   depends_on = [module.ec2_public]
   connection {
     type = "ssh"
-    host = "${outputs.ec2_bastion_public_ip}"
+    host = "${module.ec2_public.public_ip}"
     user = "ec2-user"
     password = ""
     private_key = file("private-key/k8s.pem")
@@ -17,10 +17,5 @@ resource "null_resource" "name" {
     inline = [
         "sudo chmod 400 /tmp/k8s.pem"
     ]
-  }
-
-  provisioner "local-exec" {
-    command = "echo vpc created on `date` and vpc id : ${data.terraform_remote_state.vpc.outputs.vpc_id} >> creation-time-vpc-id.txt"
-    working_dir = "local-exec-output-files/"
-  }
+  } 
 }
