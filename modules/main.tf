846b3a95798a05b6fa9e64d8e168bf2f700d54e0
vishnuk9 <vishnuacharya99.com>
2023-02-02 21:12:53 +0530

    a

--- modules/main.tf
+++ modules/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   vpc_cidr = "10.123.0.0/16"
 }
 
-module "securoty-group" {
+module "security-group" {
   source = "./security-groups/"
   vpc_id = module.vpc.vpc_id
 }
--- modules/security-groups/variable.tf
+++ modules/security-groups/variable.tf
@@ -1,3 +1,6 @@
+variable "vpc_id" {
+}
+
 variable "access_ip" {
   type = list[string]
   default = ["0.0.0.0/0"]
