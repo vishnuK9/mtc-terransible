e94f42354ef58f3dd58ea24fc4456e92ea59e699
vishnuk9 <vishnuacharya99.com>
2023-02-02 20:06:41 +0530

    a

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -62,7 +62,7 @@ resource "aws_subnet" "mtc_public_subnet" {
   vpc_id = aws_vpc.mtc_vpc.id
   cidr_block = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone = azs[count.index]
+  availability_zone = local.azs[count.index]
 
   tags = {
     Name = "mtc-public-${count.index + 1}"
@@ -75,7 +75,7 @@ resource "aws_subnet" "mtc-private" {
   vpc_id = aws_vpc.mtc_vpc.id
   cidr_block = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
-  availability_zone = azs[count.index]
+  availability_zone = local.azs[count.index]
   tags = {
     Name = "mtc-private-${count.index + 1}"
   }
