277e68096c9843ef86766e361c5935df7d23cdcf
vishnuk9 <vishnuacharya99.com>
2023-02-03 00:00:34 +0530

    aa

--- modules/main.tf
+++ modules/main.tf
@@ -4,13 +4,13 @@ module "vpc" {
   vpc_cidr = "10.123.0.0/16"
 }
 
-module "security-group" {
+module "security_group" {
   source = "./security-groups/"
   vpc_id = module.vpc.vpc_id
 }
 
 module "ec2" {
   source = "./ec2/"
-  vpc_security_group_ids = module.security-group.security_group_id
+  vpc_security_group_ids = module.security_group.security_group_id
   subnet_id = module.vpc.subnet_ids
 }
