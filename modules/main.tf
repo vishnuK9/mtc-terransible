5c4c0eca04b9644c3076c2deadbee16a6d642603
vishnuk9 <vishnuacharya99.com>
2023-02-03 02:33:35 +0530

    aa

--- modules/main.tf
+++ modules/main.tf
@@ -5,13 +5,13 @@ module "vpc" {
   nat_gateway_needed = true
 }
 
-module "security-group" {
-  source = "./security-groups/"
-  vpc_id = module.vpc.vpc_id
-}
+# module "security-group" {
+#  source = "./security-groups/"
+#  vpc_id = module.vpc.vpc_id
+# }
 
-module "ec2" {
-  source = "./ec2/"
-  vpc_security_group_ids = module.security-group.security_group_id
-  subnet_id = module.vpc.subnet_ids
-}
+# module "ec2" {
+#  source = "./ec2/"
+#  vpc_security_group_ids = module.security-group.security_group_id
+#  subnet_id = module.vpc.subnet_ids
+# }
